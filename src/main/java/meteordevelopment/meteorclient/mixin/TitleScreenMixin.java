5e4cb056d04b2fbebb038e05fb27f887158ae6db
Practice <115161598+LetMePractice@users.noreply.github.com>
2025-02-27 18:38:22 +0530

    Update TitleScreenMixin.java

--- src/main/java/meteordevelopment/meteorclient/mixin/TitleScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/TitleScreenMixin.java
@@ -1,38 +1,105 @@
-/*
- * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
- * Copyright (c) Meteor Development.
- */
+.class public Lmeteordevelopment/meteorclient/mixin/TitleScreenMixin;
+.super Lnet/minecraft/class_437;
+.source "TitleScreenMixin.java"
 
-package meteordevelopment.meteorclient.mixin;
 
-import meteordevelopment.meteorclient.systems.config.Config;
-import meteordevelopment.meteorclient.utils.player.TitleScreenCredits;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.text.Text;
-import org.lwjgl.glfw.GLFW;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-@Mixin(TitleScreen.class)
-public abstract class TitleScreenMixin extends Screen {
-    public TitleScreenMixin(Text title) {
-        super(title);
+# annotations
+.annotation build Lorg/spongepowered/asm/mixin/Mixin;
+    value = {
+        Lnet/minecraft/class_442;
     }
+.end annotation
 
-    @Inject(method = "render", at = @At("TAIL"))
-    private void onRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        if (Config.get().titleScreenCredits.get()) TitleScreenCredits.render(context);
-    }
 
-    @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
-    private void onMouseClicked(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> info) {
-        if (Config.get().titleScreenCredits.get() && button == GLFW.GLFW_MOUSE_BUTTON_LEFT) {
-            if (TitleScreenCredits.onClicked(mouseX, mouseY)) info.setReturnValue(true);
+# direct methods
+.method public constructor <init>(Lnet/minecraft/class_2561;)V
+    .registers 2
+
+    .line 33
+    invoke-direct {p0, p1}, Lnet/minecraft/class_437;-><init>(Lnet/minecraft/class_2561;)V
+
+    .line 34
+    return-void
+.end method
+
+.method private synthetic lambda$onRenderIdkDude$2()V
+    .registers 1
+
+    .line 45
+    return-void
+.end method
+
+.method private onMouseClicked(DDILorg/spongepowered/asm/mixin/injection/callback/CallbackInfoReturnable;)V
+    .registers 7
+    .annotation system Ldalvik/annotation/Signature;
+        value = {
+            "(DDI",
+            "Lorg/spongepowered/asm/mixin/injection/callback/CallbackInfoReturnable",
+            "<",
+            "Ljava/lang/Boolean;",
+            ">;)V"
         }
-    }
-}
+    .end annotation
+
+    .annotation runtime Lorg/spongepowered/asm/mixin/injection/Inject;
+        at = {
+            .subannotation Lorg/spongepowered/asm/mixin/injection/At;
+                value = "HEAD"
+            .end subannotation
+        }
+        cancellable = true
+        method = {
+            "mouseClicked"
+        }
+    .end annotation
+
+    .line 80
+    return-void
+.end method
+
+.method private onRender(Lnet/minecraft/class_332;IIFLorg/spongepowered/asm/mixin/injection/callback/CallbackInfo;)V
+    .registers 6
+    .annotation runtime Lorg/spongepowered/asm/mixin/injection/Inject;
+        at = {
+            .subannotation Lorg/spongepowered/asm/mixin/injection/At;
+                value = "TAIL"
+            .end subannotation
+        }
+        method = {
+            "render"
+        }
+    .end annotation
+
+    .line 75
+    return-void
+.end method
+
+.method private onRenderIdkDude(Lnet/minecraft/class_332;IIFLorg/spongepowered/asm/mixin/injection/callback/CallbackInfo;)V
+    .registers 7
+    .annotation runtime Lorg/spongepowered/asm/mixin/injection/Inject;
+        at = {
+            .subannotation Lorg/spongepowered/asm/mixin/injection/At;
+                ordinal = 0x0
+                target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)I"
+                value = "INVOKE"
+            .end subannotation
+        }
+        method = {
+            "render"
+        }
+    .end annotation
+
+    .line 38
+    sget-boolean v0, Lmeteordevelopment/meteorclient/utils/Utils;->firstTimeTitleScreen:Z
+
+    if-eqz v0, :cond_7
+
+    .line 39
+    const/4 v0, 0x0
+
+    sput-boolean v0, Lmeteordevelopment/meteorclient/utils/Utils;->firstTimeTitleScreen:Z
+
+    .line 71
+    :cond_7
+    return-void
+.end method
